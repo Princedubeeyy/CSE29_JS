fdfa27921e8d6625baec3e61525d122eab1cabcc
knowledge <xyz@gmail.com>
2026-02-25 14:49:52 +0530

    day2

---
+++ script.js
@@ -0,0 +1,11 @@
+<script type ="text/javascript">
+const Symbol1 = Symbol(4)
+console.log(Symbol1);
+const Symbol2 = Symbol(4);
+console.log(Symbol2);
+if(Symbol1 === Symbol2){
+    console.log("true")
+}
+else{
+    console.log("false")
+}</script>
