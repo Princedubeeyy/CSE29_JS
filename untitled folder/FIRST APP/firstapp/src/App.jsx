8b5df57df94337538e8660a77d9f032c894c78d7
knowledge <xyz@gmail.com>
2026-04-21 23:40:52 +0530

    java

---
+++ untitled folder/FIRST APP/firstapp/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import Student from './componets/Student'
+
+function App() {
+  
+  
+
+  return (
+    <>
+      <Student name={'prince'} age={20} />
+    </>
+  )
+}
+
+export default Student
